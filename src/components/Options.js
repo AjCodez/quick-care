088257298ce13dd4bcd9c8e4cb47ac67cb6198e8
Anshuman Srivastava <anshumansri12@gmail.com>
2022-11-18 22:38:46 +0530

    updated

--- src/components/Options.js
+++ src/components/Options.js
@@ -7,7 +7,7 @@ const Options=({
     return (
       <div class="optionComp">
         <div class="card" >
-        <img src="..." class="card-img-top" alt="..." />
+        <img src={require("../resources/cont-img.jpeg")} class="card-img-top" alt="..." />
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
