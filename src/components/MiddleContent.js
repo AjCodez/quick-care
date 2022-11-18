351d6e3641cc63743b54314f97a988f93811a49d
Vibhurathore09 <vibhurathoreshic@gmail.com>
2022-11-19 01:09:33 +0530

    udpated

--- src/components/MiddleContent.js
+++ src/components/MiddleContent.js
@@ -5,26 +5,26 @@ import Services from './Services'
 
 function MiddleContent() {
   return (
-    <div class = "OPTIONS">
-        <BookingComp />
+    <div class="OPTIONS">
+      <BookingComp />
 
-        <div class="options">
-          <Options name={"patient and visitors guide ➡️"} img={"../resources/cont-img.jpeg"}/>
-          <Options name={"Find the best doctor ➡️"} img={"../resources/cont-img.jpeg"}/>
-          <Options name={"Ask your. questions ➡️"} img ={"../resources/cont-img.jpeg"}/>
+      <div class="options">
+        <Options name={"patient and visitors guide ➡️"} img={"../resources/cont-img.jpeg"} />
+        <Options name={"Find the best doctor ➡️"} img={"../resources/cont-img.jpeg"} />
+        <Options name={"Ask your. questions ➡️"} img={"../resources/cont-img.jpeg"} />
 
-        </div>
+      </div>
 
-        <div className='container text-center'>
+      <div className='container text-center'>
         <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-        <Services service={"General Health Care"} desc={"dsjvbajdsjvabhjdjhbdfjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"}/>
-        <Services service={"Dental Service"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"}/>
-        <Services service={"Orthopedics"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"}/>
-        <Services service={"Rhynoplasty"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"}/>
-        <Services service={"Hair Transplant"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"}/>
-        <Services service={"FaceList Surgery"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"}/>
+          <Services service={"General Health Care"} desc={"dsjvbajdsjvabhjdjhbdfjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"} />
+          <Services service={"Dental Service"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"} />
+          <Services service={"Orthopedics"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"} />
+          <Services service={"Rhynoplasty"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"} />
+          <Services service={"Hair Transplant"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"} />
+          <Services service={"FaceList Surgery"} desc={"dsjvbajdgbsjadgjasdbvjsdgvjjhgfjadbsvjhbdsvjhadsvjbdjbvjdfbhbadjvajhdfdgbsjadgjasdbvjsdgv"} />
         </div>
-        </div>
+      </div>
     </div>
   )
 }
